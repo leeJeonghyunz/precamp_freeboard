40b5d8573db1d984a907e412cf022f76203e0e7a
leeJeonghyunz <dlwjdus3217@naver.com>
2023-11-15 17:05:27 +0900

    Reactive work(3)

--- src/components/units/hmm/hmm.presenter.tsx
+++ src/components/units/hmm/hmm.presenter.tsx
@@ -1,13 +1,20 @@
 import * as S from "./hmm.styles";
+import { useMediaQuery } from "react-responsive";
 
 export default function HmmUI(props): JSX.Element {
+  const isMobile = useMediaQuery({
+    query: "(max-width:800px)",
+  });
+
   return (
-    <S.Wrapper>
+    <S.Wrapper isMobile={isMobile}>
       <div>
-        <S.Dog src={props.dog} />
+        <S.Dog isMobile={isMobile} src={props.dog} />
       </div>
-      <S.ButtonDiv>
-        <S.ResetBtn onClick={props.onClickReload}>다시뽑기!</S.ResetBtn>
+      <S.ButtonDiv isMobile={isMobile}>
+        <S.ResetBtn isMobile={isMobile} onClick={props.onClickReload}>
+          다시뽑기!
+        </S.ResetBtn>
       </S.ButtonDiv>
     </S.Wrapper>
   );
--- src/components/units/hmm/hmm.styles.ts
+++ src/components/units/hmm/hmm.styles.ts
@@ -1,27 +1,32 @@
 import styled from "@emotion/styled";
 
+interface IProps {
+  isMobile: boolean;
+}
+
 export const Wrapper = styled.div`
   display: flex;
+  width: 100%;
+  flex-direction: ${(props: IProps) => (props.isMobile ? "column" : "row")};
 `;
 
 export const Dog = styled.img`
   height: 500px;
-  width: 500px;
+  width: ${(props: IProps) => (props.isMobile ? "100%" : "500px")};
 `;
 
 export const ButtonDiv = styled.div`
   width: 100%;
-  height: 500px;
   margin-right: 50px;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: ${(props: IProps) => (props.isMobile ? "" : "center")};
 `;
 
 export const ResetBtn = styled.button`
-  width: 250px;
-  height: 250px;
-  border-radius: 50%;
+  width: ${(props: IProps) => (props.isMobile ? "100%" : "250px")};
+  height: ${(props: IProps) => (props.isMobile ? "50px%" : "250px")};
+  border-radius: ${(props: IProps) => (props.isMobile ? "%" : "50%")};
   background-color: red;
   color: white;
   cursor: pointer;
